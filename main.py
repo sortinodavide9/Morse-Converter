bfbf5ce6377105493d3683470a8c8a785f4f8ae4
sortinodavide9 <sortinodavide9@gmail.com>
2020-12-06 19:28:57 +0100

    aggiunte

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame as pg
+import sys
 from pygame import *
 pg.init()
 #Finestra:
@@ -8,23 +9,54 @@ screen = pg.display.set_mode(WINDOW_SIZE,0,0)
 clock = pg.time.Clock()
 #Testo:
 base_font = pg.font.Font(None,32)
-user_text = ""
+textList = [""]
+listCounter = 0
+accapoCounter = 0
+
 while True:
     clock.tick(60)
+    screen.fill((255,0,0)) 
     #POLL EVENTS
     for event in pg.event.get():
         if event.type == QUIT:
             pg.quit()
             sys.exit()
         if event.type == KEYDOWN:
-            user_text+=event.unicode
+            listCounterTemp = listCounter
+            accapoCounter += 1
+            textList[listCounterTemp] += event.unicode #immissione del testo
             if event.key == K_ESCAPE:
                 pg.quit()
                 sys.exit()
-        
-        #Draw & Update:     
-        textSurface = base_font.render(user_text,True,(255,255,255))
-        screen.blit(textSurface,(0,0))
-        pg.display.update()
+            elif event.key == K_RIGHT:
+                    if(len(textList[listCounterTemp]) >= 1):
+                        accapoCounter -= 2
+                        #Cancellazione di un carattere:
+                        temp = textList[listCounterTemp]
+                        textList[listCounterTemp] = ""
+                        for characters in range(len(temp)-1):
+                            textList[listCounterTemp] += temp[characters] 
+                    else:
+                        listCounterTemp -= 1
+                        if(len(textList[listCounterTemp]) >= 1):
+                            accapoCounter -= 2
+                            #Cancellazione di un carattere:
+                            temp = textList[listCounterTemp]
+                            textList[listCounterTemp] = ""
+                            for characters in range(len(temp)-1):
+                                textList[listCounterTemp] += temp[characters]
+    if(accapoCounter >= 45):
+        listCounter += 1
+        textList.append("")
+        accapoCounter = 0
+    #Draw & Update:
+    posy = 0   
+    for text in textList:
+        textSurface = base_font.render(text,True,(255,255,255))
+        screen.blit(textSurface,(0,posy))
+        posy += 15
+    #Pulsante di stampa:
+    pg.draw.rect(screen, (2,30,33),(WINDOW_SIZE[0]-170, WINDOW_SIZE[1]-65,150,50))
+    pg.display.update()
     
         
